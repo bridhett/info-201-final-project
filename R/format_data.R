11ab92db8e89b4e83eaabc9d4e5a5f36187c1bc4
Bridhett Puerto-Ulloa <bridhett17@yahoo.com>
2019-05-29 13:38:48 -0700

    updated map function

---
+++ R/format_data.R
@@ -0,0 +1,36 @@
+data_2010 <- read.csv("data/FatalCrashData2010.csv")
+data_2011 <- read.csv("data/FatalCrashData2011.csv")
+data_2012 <- read.csv("data/FatalCrashData2012.csv")
+data_2013 <- read.csv("data/FatalCrashData2013.csv")
+data_2014 <- read.csv("data/FatalCrashData2014.csv")
+data_2015 <- read.csv("data/FatalCrashData2015.csv")
+data_2016 <- read.csv("data/FatalCrashData2016.csv")
+data_2017 <- read.csv("data/FatalCrashData2017.csv")
+
+names(data_2010) <- c("state", "<5", "5-9", "10-15", "16-20", "21-24", "25-34", 
+                      "35-44", "45-54", "55-64", "65-74", ">74", "unknown", 
+                      "total_killed") 
+names(data_2011) <- c("state", "<5", "5-9", "10-15", "16-20", "21-24", "25-34", 
+                      "35-44", "45-54", "55-64", "65-74", ">74", "unknown", 
+                      "total_killed") 
+names(data_2012) <- c("state", "<5", "5-9", "10-15", "16-20", "21-24", "25-34", 
+                      "35-44", "45-54", "55-64", "65-74", ">74", "unknown", 
+                      "total_killed") 
+names(data_2013) <- c("state", "<5", "5-9", "10-15", "16-20", "21-24", "25-34", 
+                      "35-44", "45-54", "55-64", "65-74", ">74", "unknown", 
+                      "total_killed") 
+names(data_2014) <- c("state", "<5", "5-9", "10-15", "16-20", "21-24", "25-34", 
+                      "35-44", "45-54", "55-64", "65-74", ">74", "unknown", 
+                      "total_killed") 
+names(data_2015) <- c("state", "<5", "5-9", "10-15", "16-20", "21-24", "25-34", 
+                      "35-44", "45-54", "55-64", "65-74", ">74", "unknown", 
+                      "total_killed") 
+names(data_2016) <- c("state", "<5", "5-9", "10-15", "16-20", "21-24", "25-34", 
+                      "35-44", "45-54", "55-64", "65-74", ">74", "unknown", 
+                      "total_killed") 
+names(data_2017) <- c("state", "<5", "5-9", "10-15", "16-20", "21-24", "25-34", 
+                      "35-44", "45-54", "55-64", "65-74", ">74", "unknown", 
+                      "total_killed") 
+
+
+
