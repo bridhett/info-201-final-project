84416c7ed4541cefc99f7478cf51b0426b151c7d
Israel Martinez <israelma@uw.edu>
2019-06-04 19:38:44 -0700

    Completed project

--- Final_Shiny/ui.R
+++ Final_Shiny/ui.R
@@ -17,8 +17,15 @@ shinyUI(navbarPage("Fatal Car Crashes in the USA",
          tabPanel("Nation Wide",
                   fluidRow(includeCSS("style.css"),
                            column(12,
-                                  h1("Nation Wide Fatal Car Crashes"),
-                                  p("The map "))),
+                                  h1("Nation Wide Fatal Car Crashes")),
+                           column(12,
+                                  p("To utilize the first map plot, first choose the year you are interested in. 
+                                  Then you can hover over a state with your mouse to find the total amount of kills 
+                                  in that year. In the second map plot, choose the year and age group and then you 
+                                  will be able to see the U.S. states color coded by how many fatalities they had 
+                                  for that age group in that year. In both maps, Green indicates a low fatality 
+                                  number and a fading into red indicates higher fatalities. This can be important 
+                                  to use if you want to see the most dangerous places for a specific age group."))),
                   hr(),
                   
                   fluidRow(sidebarPanel(
@@ -51,7 +58,12 @@ shinyUI(navbarPage("Fatal Car Crashes in the USA",
          tabPanel("State Wide",
                   fluidRow(column(12,
                                 h1("Fatal Car Crashes Per State"),
-                                p("A bar graph showing number of death in different years and states based on age groups."))),
+                                p("To utilize this bar graph, first choose the year you are interested in and 
+                                the state. In the bar graph you will see the number of car crash deaths in 
+                                the state for each age group of the year. A red colored bar indicates that 
+                                the number is above the national average while the green colored bar 
+                                indicates that it's below average. This can be important to use to find out 
+                                  which age groups suffer the most deaths from car crashes."))),
                   hr(),
                   fluidRow(sidebarPanel(
                     sliderInput("years",
@@ -69,6 +81,14 @@ shinyUI(navbarPage("Fatal Car Crashes in the USA",
          ),
          
          tabPanel("Over Time",
+                  fluidRow(column(12,
+                                   h1("Fatal Car Crashes Over time"),
+                                   p("To utitlize this line graph, first choose the state you are interested in 
+                                   and the age group. In the line graph, you will see how the number of 
+                                   fatalities progress throughout the years. This can be important to use to 
+                                   find out whether a state's fatality rate is improving or getting worse as 
+                                     time goes by for the selected age group."))),
+                  hr(),
                   fluidRow(sidebarPanel(
                     selectInput(inputId = "selected_state", 
                                 label = "Choose a State:", 
@@ -123,8 +143,19 @@ shinyUI(navbarPage("Fatal Car Crashes in the USA",
                                mainPanel(h2("State Rankings Per Year"),tableOutput("ranks")))
                              ),
                     
-                    tabPanel("Project Conclusion"))
-                    
+                    tabPanel("Project Conclusion",
+                             fluidRow(column(12,
+                                            h1("Mission Statement"),
+                                            hr(),
+                                            p("The data that we are working with are 8 data sets (2010-2017) from the National 
+                                              Highway Traffic Safety Administrations (NHTSA). These tell us the number of 
+                                              people killed in fatal car crashes based on age group and US state.\n
+                                              Our mission for our data that we report is to give insight to people who may be
+                                              affected by vehicle laws in their state. We hope that this information plays as a
+                                              determining factor on whether residents stay in/move to a certain state or not, 
+                                              especially families. Also, we hope that legislators and lawmakers make use of our
+                                              data for evidence to improve vehicle laws in their representing area."))))
+                             )
 
 ))
 
---
+++ summary_stats.R
@@ -0,0 +1,30 @@
+
+p("The data that we are working with are 8 data sets (2010-2017) from the National 
+  Highway Traffic Safety Administrations (NHTSA). These tell us the number of 
+  people killed in fatal car crashes based on age group and US state.")
+
+p("Our mission for our data that we report is to give insight to people who may be
+  affected by vehicle laws in their state. We hope that this information plays as a
+  determining factor on whether residents stay in/move to a certain state or not, 
+  especially families. Also, we hope that legislators and lawmakers make use of our
+  data for evidence to improve vehicle laws in their representing area.")
+
+Map
+"To utilize this map plot, first choose the year you are interested in and then choose 
+an age group. Now, you will be able to see a the U.S. states color coded by how many 
+fatalities they had for that age group in that year. Light yellow indicates a low 
+fatality number and the darker the state, the higher the fatalities. This can be 
+important to use if you want to see the most dangerous places for a specific age group."
+
+Bar graph
+"To utilize this bar graph, first choose the year you are interested in and the state. 
+In the bar graph you will see the number of car crash deaths in the state for each age 
+group of the year. A red colored bar indicates that the number is above the national 
+average while the green colored bar indicates that it's below average. This can be 
+important to use to find out which age groups suffer the most deaths from car crashes."
+
+Line graph
+"To utitlize this line graph, first choose the state you are interested in and the age 
+group. In the line graph, you will see how the number of fatalities progress throughout 
+the years. This can be important to use to find out whether a state's fatality rate is 
+improving or getting worse as time goes by for the selected age group."
